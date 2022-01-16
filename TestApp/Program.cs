ad706cd6070bdf3aea9b29dfdf176efc91d910cf
Ondřej Václavek <vaclavek@havit.cz>
2022-01-16 10:34:07 +0100

    Use MyTask

--- TestApp/Program.cs
+++ TestApp/Program.cs
@@ -1,2 +1,23 @@
-﻿Console.WriteLine("Done");
-Console.ReadLine();
+﻿using TaskLibrary;
+
+Console.WriteLine("Start");
+
+var task = AsyncMethod();
+task.Wait();
+
+Console.WriteLine("Done");
+
+static MyTask AsyncMethod()
+{
+	var tcs = new MyTaskCompletionSource();
+
+	var thread = new Thread(() =>
+	{
+		Thread.Sleep(1000);
+		tcs.Complete();
+	});
+
+	thread.Start();
+
+	return tcs.Task;
+}
