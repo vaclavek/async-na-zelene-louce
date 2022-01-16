e5bb860db6cdeb2fe524f5da3a7800d69768213a
Ondřej Václavek <vaclavek@havit.cz>
2022-01-16 10:31:06 +0100

    Add MyTaskCompletionSource

---
+++ TaskLibrary/MyTaskCompletionSource.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace TaskLibrary
+{
+	public class MyTaskCompletionSource
+	{
+		public MyTask Task { get; }
+
+		public MyTaskCompletionSource()
+		{
+			Task = new();
+		}
+
+		public void Complete()
+		{
+			Task.IsCompleted = true;
+		}
+	}
+}
